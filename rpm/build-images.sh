74a749bfec0a816e8b94a9392ab592fbb67001ec
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2016-02-13 10:09:16 +0100

    Grab latest centos image before image build

--- rpm/build-images.sh
+++ rpm/build-images.sh
@@ -3,5 +3,7 @@ set -ex
 tags=${tags:-"centos5 centos6 centos7"}
 
 for t in ${tags}; do
+    # Grab latest centos image
+    docker pull centos:${t}
     docker build -t italiangrid/pkg.base:${t} -f Dockerfile.${t} .
 done
