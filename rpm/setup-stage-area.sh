450c8e49c8ac4e886cbf465fdfc38b2c587fabf7
enricovianello <maverick17586@gmail.com>
2017-07-10 18:00:48 +0200

    Used a different stage_area variables set and directory for srpms

--- rpm/setup-stage-area.sh
+++ rpm/setup-stage-area.sh
@@ -10,17 +10,31 @@ fi
 cat << EOF > /etc/yum.repos.d/build-stage-area.repo
 [build-stage-area]
 name=build-stage-area
-baseurl=file://${stage_area_dir}/rpms
+baseurl=file://${stage_area_dir}
 protect=1
 enabled=1
 priority=1
 gpgcheck=0
 EOF
 
-mkdir -p ${stage_area_dir}/rpms
+mkdir -p ${stage_area_dir}
 
 if [ ! -d "${stage_area_dir}/repodata" ]; then
-  createrepo ${stage_area_dir}/rpms
+  createrepo ${stage_area_dir}
   chown -R build:build ${stage_area_dir}
 fi
 
+## stage area for SRPMS
+
+stage_area_dir_srpms=/stage-area-srpms/${BUILD_PLATFORM}
+
+if [ -n "${PKG_STAGE_DIR_SRPMS}" ]; then
+  stage_area_dir_srpms=${PKG_STAGE_DIR_SRPMS}
+fi
+
+mkdir -p ${stage_area_dir_srpms}
+
+if [ ! -d "${stage_area_dir_srpms}/repodata" ]; then
+  createrepo ${stage_area_dir_srpms}
+  chown -R build:build ${stage_area_dir_srpms}
+fi
