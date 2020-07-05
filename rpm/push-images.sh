80fd4c77d633d457510adbc776f37886d59fa7de
enricovianello <maverick17586@gmail.com>
2020-07-05 13:59:37 +0200

    removed push to registry

--- rpm/push-images.sh
+++ rpm/push-images.sh
@@ -2,12 +2,12 @@
 set -ex
 tags=${tags:-"centos6 centos6devtoolset7 centos7 centos8"}
 
-if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
-  for t in ${tags}; do
-    docker tag italiangrid/pkg.base:${t} ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.base:${t}
-    docker push ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.base:${t}
-  done
-fi
+#if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
+#  for t in ${tags}; do
+#    docker tag italiangrid/pkg.base:${t} ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.base:${t}
+#    docker push ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.base:${t}
+#  done
+#fi
 
 if [ -n "${PUSH_TO_DOCKERHUB}" ]; then
   for t in ${tags}; do
