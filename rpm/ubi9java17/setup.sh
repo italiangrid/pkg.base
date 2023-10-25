2390bdbc691ff23b0492c30afac615ce0fa1e0f5
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2023-10-25 10:59:36 +0200

    Fix broken subscription

--- rpm/ubi9java17/setup.sh
+++ rpm/ubi9java17/setup.sh
@@ -9,8 +9,7 @@ BUILD_USER_HOME=${BUILD_USER_HOME:-/home/${BUILD_USER}}
 echo "include_only=.garr.it,.cern.ch" >> /etc/yum/pluginconf.d/fastestmirror.conf
 
 # Instal EPEL release
-subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 
 yum clean all
 yum install -y hostname
