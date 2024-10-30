c133daa101cfb751018076414553352507ad1907
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2024-10-30 22:27:19 +0100

    Fix builded image

--- rpm/build-images.sh
+++ rpm/build-images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
-tags=${tags:-"centos7 centos7java11 centos7java17"}
+tags=${tags:-"almalinux9base almalinux9java8 almalinux9java11 almalinux9java17"}
 docker_opts=${docker_opts:-"--rm=true --no-cache=true"}
 
 for t in ${tags}; do
     docker build \
       ${docker_opts} \
-      -t italiangrid/pkg.base:${t} -f Dockerfile.${t} .
+      -t cnafsd/pkg.base:${t} -f Dockerfile.${t} .
 done
